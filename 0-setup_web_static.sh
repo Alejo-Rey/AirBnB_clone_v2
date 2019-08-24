99ed139fc116df931643c1aff05c6708c2447825
Alejo-rey <771@holbertonschool.com>
2019-08-24 04:39:56 +0000

    [FEATURE] create a bash to configure path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,6 +14,6 @@ echo -e '<html>
 </html>' | sudo tee '/data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static/;\n\t}' /etc/nginx/sites-enabled/default
 sudo service nginx reload
 sudo service nginx restart
