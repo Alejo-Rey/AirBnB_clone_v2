3c760443684e68fb9e4ffbb459677f953a957f70
Alejo-rey <771@holbertonschool.com>
2019-08-24 04:55:10 +0000

    [FEATURE] create a bash to configure path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,13 +5,7 @@ sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 sudo touch /data/web_static/releases/test/index.html
-echo -e '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' | sudo tee '/data/web_static/releases/test/index.html'
+echo 'Holberton School' | sudo tee '/data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/hbnb_static/;\n\t}' /etc/nginx/sites-enabled/default
