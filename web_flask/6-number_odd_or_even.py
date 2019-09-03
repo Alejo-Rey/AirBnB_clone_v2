76f48adfc0f54d6a50aa9ac30f917e73ff735232
Alejo-rey <771@holbertonschool.com>
2019-09-03 23:09:30 +0000

    [FEATURE] script to check if is is even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+''' funtion using flask '''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    ''' send a index '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' routing path '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c():
+    ''' variable routing '''
+    hi = 'C {}'.format(text.replace('_', ' '))
+    return hi
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py(text='is cool'):
+    ''' pre cool '''
+    hi = 'Python {}'.format(text.replace('_', ' '))
+    return hi
+
+
+@app.route('/number/<int:n>')
+def num(n):
+    ''' display only if is a numer '''
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def tem_num(n):
+    ''' give a template of junja2 '''
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def tem_num2(n):
+    ''' give a template of junja2 if even or odd'''
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
