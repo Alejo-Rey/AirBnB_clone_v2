f7865d3c57958351c84894ecff92b54b8a318b9c
Alejo-rey <771@holbertonschool.com>
2019-09-03 19:58:41 +0000

    [FEATURE] scritp to routing and display a default variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+''' funtion using flask '''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    ''' send a index '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' routing path '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c():
+    ''' variable routing '''
+    hi = 'C {}'.format(text.replace('_', ' '))
+    return hi
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py(text='is cool'):
+    ''' pre cool '''
+    hi = 'Python {}'.format(text.replace('_', ' '))
+    return hi
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
